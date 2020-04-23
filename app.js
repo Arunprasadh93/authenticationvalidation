84bd6d7c6bb762f2d04e14ce9396d986abb1a295
Arun Prasadh <arunprasadh.sj@gmail.com>
2020-04-23 15:02:29 +0530

    authenticate and excrypt using MD5 then Bcrypt

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
+
+
+
+
+
+const saltRounds = 10;
+
+
+
 
 
 app =express();
@@ -31,27 +42,30 @@ const userSchema = new mongoose.Schema({
 });
 
 
+//
+// userSchema.plugin(encrypt , {secret : process.env.SECRET , encryptedFields : ['password'] } );
 
-userSchema.plugin(encrypt , {secret : process.env.SECRET , encryptedFields : ['password'] } );
+
 
 const User = new mongoose.model("User" , userSchema );
 
 
 app.post("/register" , function(req , res) {
 
-  const newUser = new User({
-    email : req.body.username,
-    password : req.body.password
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email : req.body.username,
+      password : hash
+    });
+    newUser.save(function(err){
 
-  newUser.save(function(err){
-
-    if(!err) {
-      res.render("secrets");
-    }
-    else {
-      console.log(err);
-    }
+      if(!err) {
+        res.render("secrets");
+      }
+      else {
+        console.log(err);
+      }
+    });
   });
 });
 
@@ -66,10 +80,14 @@ app.post("/login" , function(req , res) {
       }
       else {
         if(foundUser) {
-          if(foundUser.password === password)
-          {
-            res.render("secrets");
-          }
+          bcrypt.compare(password, foundUser.password, function(err, result) {
+            if(result === true)
+            {
+              res.render("secrets");
+            }
+          });
+
+
         }
       }
     });
